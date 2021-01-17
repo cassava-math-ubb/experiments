5cf99aa37663040bca71b5616d82b665227c2d20
alex_darie <alexdarieph@gmail.com>
2021-01-18 00:07:55 +0200

    metrics are working, must work on time complexity, avoid multiple IO ops

--- data_analysis/mc_simulations/mc_structural_similarity.py
+++ data_analysis/mc_simulations/mc_structural_similarity.py
@@ -7,18 +7,19 @@ import json
 import cv2
 import lpips as spipl
 
+import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
 from tabulate import tabulate
 
-"""
-	Some basic loading for the images, resulting in an ordinary Python dict with the following
-	strcutre: {'image_name.jpg': {'image': <RGB-3channel>, 'label': <integer>}}
-"""
+print(tf.__version__)
+
+
 class LoadImageDataset:
 	def __init__(self, base_path, train_dir, test_dir, limit = 10):
+		self.base_path = base_path
 		self.limit = limit
 		self.imgnames = []
 		self.train_dir = base_path + '/' + train_dir
@@ -42,94 +43,69 @@ class LoadImageDataset:
 			line = line.strip('\n')
 			bucket = line.split(',')
 			bucket = [el.strip(' \n') for el in bucket]
-			train_images[bucket[0]] = {
-				'image': np.array(cv2.imread(self.train_dir + '/' + bucket[0])),
-				'label': int(bucket[1])
+			train_images[bucket[1]] = {
+				'image': bucket[0] # tf.io.decode_jpeg(tf.io.read_file(self.base_path + '/train_images/' + bucket[0]))
 			}
 			self.imgnames.append(bucket[0])
 
 		return train_images
 
 
-"""
-	The Image Similarity Metric conducts a Monte Carlo simulation on a randomly selected subset 
-	of k images with the final purpose of determining the similarity between the images contained 
-	in the training folder.
-
-	This class makes use of the following standard metrics definitions:
-		a. LPIPS: Learned Perceptual Image Patch Similarity 
-		   (make sure to run: pip3 install lpips)
-		b. PNSR: Peak signal-to-noise ratio
-		c. SSIM: Structural similarity index measure
-
-"""
-class ISM:
+class StudyStructuralSimilarity:
 	def __init__(self):
 		self.lpips = 0
 		self.psnr = 0
 		self.ssim = 0
+		self.imgs_limit = 50
 		self.imgpack = LoadImageDataset('/Volumes/LaCie SSD/cassava-leaf-disease-classification', 
-						'train_images', 'test_images', 100)
-		self._run(sample_size = 10, num_simulations = 100)
+						'train_images', 'test_images', self.imgs_limit)
+		self._run(sample_size = 5, num_simulations = 1)
 
 
 	def overview(self):
 		print('Overview: ')
 		print(tabulate([['LPIPS', self.lpips], 
-			['PNSR', self.psnr], 
+			['PSNR', self.psnr], 
 			['SSIM', self.ssim]], 
 			headers=['Metric', 'Value']))
 
 
 	def _run(self, sample_size, num_simulations):
-		"""
-			To perform an experiment, we need to compute the similarity between each
-			(i, j) pair of images, and average the results. For each such pair we will
-			compute all three metrics, and return the average of those individually.
-		"""
-		images = self._rand_subsample(sample_size)
-
+		
 		def generate_pairs(sample_size):
 			pairs = []
 
-			""" Generate the pairs. """
 			for i in range(sample_size):
-				for j in range(i, sample_size):
+				for j in range(i + 1, sample_size):
 					pairs.append([i, j])
 
 			return pairs
-
+		
 		pairs = generate_pairs(sample_size)
-
 		trails_results = {'lpips': [], 'psnr': [], 'ssim': []}
+		
 		for i in range(num_simulations):
-			l, p, s = self._simulation(pairs)
+			random_imgnames = self._rand_subsample(sample_size)
+			l, p, s = self._simulation(pairs, random_imgnames)
 			trails_results['lpips'].append(l)
 			trails_results['psnr'].append(p)
 			trails_results['ssim'].append(s)
+			print('### simulation ' + str(i + 1) + ': ' + str(l) + ', ' + str(p) + ', ' + str(s))
 
 		self.lpips = np.mean(np.array(trails_results['lpips']))
 		self.psnr = np.mean(np.array(trails_results['psnr']))
 		self.ssim = np.mean(np.array(trails_results['ssim']))
 
 
-	def _simulation(self, pairs):
-		"""
-			An experiment is defined as computing the similarity index for every
-			pair (i, j) of images using the metrics defined above.
-		"""
+	def _simulation(self, pairs, random_imgnames):
 		experiment_results = {'lpips': [], 'psnr': [], 'ssim': []}
 		for pair in pairs:
-			name1 = self.imgpack.imgnames[pair[0]]
-			name2 = self.imgpack.imgnames[pair[1]]
-			img1 = self.imgpack.images[name1]['image']
-			img2 = self.imgpack.images[name2]['image']
-			
-			""" call in the other module """
+			name_a = self.imgpack.base_path + '/train_images/' + random_imgnames[pair[0]]
+			name_b = self.imgpack.base_path + '/train_images/' + random_imgnames[pair[1]]
 
 			l = 0
-			p = 0 
-			s = 0
+			p = self.compute_psnr(name_a, name_b)
+			s = self.compute_ssim(name_a, name_b)
 			experiment_results['lpips'].append(l)
 			experiment_results['psnr'].append(p)
 			experiment_results['ssim'].append(s)
@@ -141,37 +117,67 @@ class ISM:
 		return lpips, psnr, ssim
 
 
-	def _rand_subsample(self, sample_size, debug = False):
-		"""
-			Using U, an uniformly distributed random variable, randomly select
-			a number of images (sample_size) so that we can conduct a similarity
-			experiemnt on them.
+	def compute_psnr(self, name_a, name_b):
+		im1 = tf.io.read_file(name_a)
+		im2 = tf.io.read_file(name_b)
 
-		"""
+		im1 = tf.io.decode_jpeg(im1)
+		im2 = tf.io.decode_jpeg(im2)
 
-		if debug == True:
-			print('f: _rand_subsample(' + str(sample_size) + ', True)')
+		# Compute PSNR over tf.uint8 Tensors.
+		psnr1 = tf.image.psnr(im1, im2, max_val=255)
 
-		n = len(self.imgpack.imgnames)
+		# Compute PSNR over tf.float32 Tensors.
+		im1 = tf.image.convert_image_dtype(im1, tf.float32)
+		im2 = tf.image.convert_image_dtype(im2, tf.float32)
+		psnr2 = tf.image.psnr(im1, im2, max_val=1.0)
+
+		return psnr2.numpy()
+
+
+	def compute_ssim(self, name_a, name_b):
+		im1 = tf.io.read_file(name_a)
+		im2 = tf.io.read_file(name_b)
+
+		# Read images from file.
+		im1 = tf.io.decode_jpeg(im1)
+		im2 = tf.io.decode_jpeg(im2)
+
+		# Compute SSIM over tf.uint8 Tensors.
+		ssim1 = tf.image.ssim(im1, im2, max_val=255, filter_size=11,
+		                      filter_sigma=1.5, k1=0.01, k2=0.03)
+
+		# Compute SSIM over tf.float32 Tensors.
+		im1 = tf.image.convert_image_dtype(im1, tf.float32)
+		im2 = tf.image.convert_image_dtype(im2, tf.float32)
+		ssim2 = tf.image.ssim(im1, im2, max_val=1.0, filter_size=11,
+		                      filter_sigma=1.5, k1=0.01, k2=0.03)
+
+		return ssim2.numpy()
+
+
+	def _rand_subsample(self, sample_size):
+		# Takes any cassava leaf images class.
 		imgnames = []
-		images = []
-		debugidxs = []
+		visited = []
 		
-		for u in np.random.uniform(0, n, sample_size):
-			debugidxs.append(int(u))
-			imgnames.append(self.imgpack.imgnames[int(u)])
-		for imgname in imgnames:
-			images.append(self.imgpack.images[imgname])
+		while len(imgnames) < sample_size:
+			u = np.random.randint(0, self.imgs_limit - 1, 1)[0]
+			if u not in visited:
+				imgnames.append(self.imgpack.imgnames[u])
+				visited.append(u)
 
-		if debug == True:
-			print('\t| gen. subsample with idxs: ', end='')
-			print(debugidxs)
-		return images
+		return imgnames
+
+
+	def _specific_cassava_class_rand_subsample(self, sample_size, cassava_class):
+		# Takes the given cassava leaf images class into consideration.
+		pass
 
 
 def cassava_data_analysis():
-	ism = ISM()
-	ism.overview()
+	study = StudyStructuralSimilarity()
+	study.overview()
 
 
 cassava_data_analysis()
