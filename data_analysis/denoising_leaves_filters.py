50a7f5f69f12261219963131d2db85c36d85aaf0
alex_darie <alexdarieph@gmail.com>
2021-01-18 17:58:44 +0200

    hate typos

--- data_analysis/denoising_leaves_filters.py
+++ data_analysis/denoising_leaves_filters.py
@@ -4,13 +4,13 @@ from skimage import filters, exposure, io, restoration
 from skimage.color import rgb2gray
 
 def denoising_filters(cassava_leaves):
-	gaussian_filter_coins = filters.gaussian(cassava_leafs, sigma=2)
-	med_filter_coins = filters.median(cassava_leafs, np.ones((3, 3)))
-	tv_filter_coins = restoration.denoise_tv_chambolle(cassava_leafs, weight=0.1)
+	gaussian_filter_leaves = filters.gaussian(cassava_leaves, sigma=2)
+	med_filter_leaves = filters.median(cassava_leaves, np.ones((3, 3)))
+	tv_filter_leaves = restoration.denoise_tv_chambolle(cassava_leaves, weight=0.1)
 
 	plt.figure(figsize=(16, 4))
 	plt.subplot(141)
-	plt.imshow(cassava_leafs, cmap='gray', interpolation='nearest')
+	plt.imshow(cassava_leaves, cmap='gray', interpolation='nearest')
 	plt.axis('off')
 	plt.title('Image')
 	plt.subplot(142)
@@ -54,5 +54,5 @@ def sep_back_forg(cassava_leaves):
 cassava_leaves = io.imread('/Volumes/LaCie SSD/cassava-leaf-disease-classification/test_images/2216849948.jpg')
 cassava_leaves = rgb2gray(cassava_leaves)
 
-# denoising_filters(cassava_leaves)
-sep_back_forg(cassava_leaves)
+denoising_filters(cassava_leaves)
+# sep_back_forg(cassava_leaves)
