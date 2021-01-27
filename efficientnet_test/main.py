b9151d6f42065b0f836c9ac8b031122c62fa9b20
teofanaenachioiu <teofanaenachioiu@gmail.com>
2021-01-27 22:07:26 +0200

    Refactoring

--- efficientnet_test/main.py
+++ efficientnet_test/main.py
@@ -3,6 +3,8 @@ from keras_preprocessing.image import ImageDataGenerator
 from tensorflow.keras import layers
 from tensorflow.keras.applications import EfficientNetB0
 from tensorflow.keras.optimizers import Adam
+import matplotlib.pyplot as plt
+import tensorflow as tf
 import pandas as pd
 import os
 
@@ -17,7 +19,7 @@ train_labels.label = train_labels.label.astype('str')
 BATCH_SIZE = 2
 STEPS_PER_EPOCH = len(train_labels) * 0.8 / BATCH_SIZE
 VALIDATION_STEPS = len(train_labels) * 0.2 / BATCH_SIZE
-EPOCHS = 20
+EPOCHS = 10
 TARGET_SIZE = 512
 
 train_datagen = ImageDataGenerator(validation_split=0.2,
@@ -63,24 +65,24 @@ def create_model():
 
     model.compile(optimizer=Adam(lr=0.001),
                   loss="sparse_categorical_crossentropy",
-                  metrics=["acc"])
+                  metrics=["accuracy"])
     return model
 
 
 model = create_model()
 model.summary()
 
-model_save = ModelCheckpoint('./best_baseline_model.h5',
+model_save = ModelCheckpoint('./best_model.h5',
                              save_best_only=True,
                              save_weights_only=True,
                              monitor='val_loss',
-                             mode='min', verbose=1)
+                             mode='min')
 early_stop = EarlyStopping(monitor='val_loss', min_delta=0.001,
                            patience=5, mode='min', verbose=1,
                            restore_best_weights=True)
 reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.3,
                               patience=2, min_delta=0.001,
-                              mode='min', verbose=1)
+                              mode='min')
 
 history = model.fit_generator(
     train_generator,
@@ -90,3 +92,32 @@ history = model.fit_generator(
     validation_steps=VALIDATION_STEPS,
     callbacks=[model_save, early_stop, reduce_lr]
 )
+
+# evaluate model
+score = model.evaluate_generator(validation_generator)
+print('Score:', score)
+
+# save model
+model_json = model.to_json()
+with open('model.json', "w") as json_file:
+    json_file.write(model_json)
+model.save_weights('weights.h5')
+
+# summarize history for accuracy
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'val'], loc='upper left')
+plt.savefig('accuracy.png')
+plt.close()
+
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'val'], loc='upper left')
+plt.savefig('loss.png')
